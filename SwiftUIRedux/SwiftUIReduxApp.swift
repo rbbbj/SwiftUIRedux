06849166765394b318439b5303054ef758ee8683
Robertas Baronas <>
2025-03-12 17:00:01 +0200

    First commit

---
+++ SwiftUIRedux/SwiftUIReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReduxApp.swift
+//  SwiftUIRedux
+//
+//  Created by Robertas Baronas on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIReduxUITests/SwiftUIReduxUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIReduxUITestsLaunchTests.swift
+//  SwiftUIReduxUITests
+//
+//  Created by Robertas Baronas on 12/03/2025.
+//
+
+import XCTest
+
+final class SwiftUIReduxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
